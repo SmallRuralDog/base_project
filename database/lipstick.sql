175cb47dfd796aaf8874932af98f263338689656
ZhangWei <296404875@qq.com>
2018-11-27 18:09:55 +0800

    sql

---
+++ database/lipstick.sql
@@ -0,0 +1,596 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2018-11-27 18:09:25
+-- 服务器版本： 10.3.8-MariaDB
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lipstick`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `admin_config`
+--
+
+CREATE TABLE `admin_config` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `admin_menu`
+--
+
+CREATE TABLE `admin_menu` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `parent_id` int(11) NOT NULL DEFAULT 0,
+  `order` int(11) NOT NULL DEFAULT 0,
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `icon` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uri` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `permission` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 转存表中的数据 `admin_menu`
+--
+
+INSERT INTO `admin_menu` (`id`, `parent_id`, `order`, `title`, `icon`, `uri`, `permission`, `created_at`, `updated_at`) VALUES
+(1, 0, 1, '首页', 'fa-bar-chart', '/', NULL, NULL, NULL),
+(2, 0, 2, '系统', 'fa-tasks', '', NULL, NULL, NULL),
+(3, 2, 3, '用户', 'fa-users', 'auth/users', NULL, NULL, NULL),
+(4, 2, 4, '角色', 'fa-user', 'auth/roles', NULL, NULL, NULL),
+(5, 2, 5, '权限', 'fa-ban', 'auth/permissions', NULL, NULL, NULL),
+(6, 2, 6, '菜单', 'fa-bars', 'auth/menu', NULL, NULL, NULL),
+(7, 2, 7, '操作记录', 'fa-history', 'auth/logs', NULL, NULL, NULL),
+(8, 13, 9, '系统日志', 'fa-database', 'logs', NULL, '2018-11-27 09:17:15', '2018-11-27 09:53:03'),
+(9, 13, 10, 'Redis管理', 'fa-database', 'redis', NULL, '2018-11-27 09:23:28', '2018-11-27 09:53:03'),
+(10, 13, 11, 'Media管理', 'fa-file', 'media', NULL, '2018-11-27 09:25:54', '2018-11-27 09:53:03'),
+(11, 13, 12, '配置', 'fa-toggle-on', 'config', NULL, '2018-11-27 09:29:16', '2018-11-27 09:53:03'),
+(12, 13, 13, '环境变量', 'fa-gears', 'env-manager', NULL, '2018-11-27 09:50:46', '2018-11-27 09:53:03'),
+(13, 0, 8, '扩展', 'fa-space-shuttle', '#', NULL, '2018-11-27 09:52:48', '2018-11-27 09:53:03');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `admin_operation_log`
+--
+
+CREATE TABLE `admin_operation_log` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `path` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `method` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ip` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `input` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 转存表中的数据 `admin_operation_log`
+--
+
+INSERT INTO `admin_operation_log` (`id`, `user_id`, `path`, `method`, `ip`, `input`, `created_at`, `updated_at`) VALUES
+(1, 1, 'admin', 'GET', '192.168.11.100', '[]', '2018-11-27 09:12:04', '2018-11-27 09:12:04'),
+(2, 1, 'admin', 'GET', '192.168.11.100', '[]', '2018-11-27 09:17:17', '2018-11-27 09:17:17'),
+(3, 1, 'admin/logs', 'GET', '192.168.11.100', '{\"_pjax\":\"#pjax-container\"}', '2018-11-27 09:17:20', '2018-11-27 09:17:20'),
+(4, 1, 'admin/auth/setting', 'GET', '192.168.11.100', '{\"_pjax\":\"#pjax-container\"}', '2018-11-27 09:18:16', '2018-11-27 09:18:16'),
+(5, 1, 'admin/auth/setting', 'GET', '192.168.11.100', '[]', '2018-11-27 09:18:34', '2018-11-27 09:18:34'),
+(6, 1, 'admin/auth/setting', 'GET', '192.168.11.100', '[]', '2018-11-27 09:18:35', '2018-11-27 09:18:35'),
+(7, 1, 'admin/auth/setting', 'GET', '192.168.11.100', '[]', '2018-11-27 09:18:37', '2018-11-27 09:18:37'),
+(8, 1, 'admin/auth/setting', 'GET', '192.168.11.100', '[]', '2018-11-27 09:19:24', '2018-11-27 09:19:24');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `admin_permissions`
+--
+
+CREATE TABLE `admin_permissions` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `http_method` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `http_path` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 转存表中的数据 `admin_permissions`
+--
+
+INSERT INTO `admin_permissions` (`id`, `name`, `slug`, `http_method`, `http_path`, `created_at`, `updated_at`) VALUES
+(1, 'All permission', '*', '', '*', NULL, NULL),
+(2, 'Dashboard', 'dashboard', 'GET', '/', NULL, NULL),
+(3, 'Login', 'auth.login', '', '/auth/login\r\n/auth/logout', NULL, NULL),
+(4, 'User setting', 'auth.setting', 'GET,PUT', '/auth/setting', NULL, NULL),
+(5, 'Auth management', 'auth.management', '', '/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs', NULL, NULL),
+(6, 'Logs', 'ext.log-viewer', NULL, '/logs*', '2018-11-27 09:17:15', '2018-11-27 09:17:15'),
+(7, 'Redis Manager', 'ext.redis-manager', NULL, '/redis*', '2018-11-27 09:23:28', '2018-11-27 09:23:28'),
+(8, 'Media manager', 'ext.media-manager', NULL, '/media*', '2018-11-27 09:25:54', '2018-11-27 09:25:54'),
+(9, 'Admin Config', 'ext.config', NULL, '/config*', '2018-11-27 09:29:16', '2018-11-27 09:29:16');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `admin_roles`
+--
+
+CREATE TABLE `admin_roles` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 转存表中的数据 `admin_roles`
+--
+
+INSERT INTO `admin_roles` (`id`, `name`, `slug`, `created_at`, `updated_at`) VALUES
+(1, 'Administrator', 'administrator', '2018-11-27 09:11:55', '2018-11-27 09:11:55');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `admin_role_menu`
+--
+
+CREATE TABLE `admin_role_menu` (
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 转存表中的数据 `admin_role_menu`
+--
+
+INSERT INTO `admin_role_menu` (`role_id`, `menu_id`, `created_at`, `updated_at`) VALUES
+(1, 2, NULL, NULL),
+(1, 13, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `admin_role_permissions`
+--
+
+CREATE TABLE `admin_role_permissions` (
+  `role_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 转存表中的数据 `admin_role_permissions`
+--
+
+INSERT INTO `admin_role_permissions` (`role_id`, `permission_id`, `created_at`, `updated_at`) VALUES
+(1, 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `admin_role_users`
+--
+
+CREATE TABLE `admin_role_users` (
+  `role_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 转存表中的数据 `admin_role_users`
+--
+
+INSERT INTO `admin_role_users` (`role_id`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `admin_users`
+--
+
+CREATE TABLE `admin_users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatar` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 转存表中的数据 `admin_users`
+--
+
+INSERT INTO `admin_users` (`id`, `username`, `password`, `name`, `avatar`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'admin', '$2y$10$moBd82L/1/hb.FXUTz6hmO3q5I0YQnCi5V2GbLur48rR779GLkb4S', 'Administrator', NULL, NULL, '2018-11-27 09:11:55', '2018-11-27 09:11:55');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `admin_user_permissions`
+--
+
+CREATE TABLE `admin_user_permissions` (
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 转存表中的数据 `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2016_01_04_173148_create_admin_tables', 2),
+(4, '2017_07_17_040159_create_config_table', 3),
+(5, '2016_06_01_000001_create_oauth_auth_codes_table', 4),
+(6, '2016_06_01_000002_create_oauth_access_tokens_table', 4),
+(7, '2016_06_01_000003_create_oauth_refresh_tokens_table', 4),
+(8, '2016_06_01_000004_create_oauth_clients_table', 4),
+(9, '2016_06_01_000005_create_oauth_personal_access_clients_table', 4);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `oauth_access_tokens`
+--
+
+CREATE TABLE `oauth_access_tokens` (
+  `id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `client_id` int(11) NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `scopes` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `revoked` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `expires_at` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `oauth_auth_codes`
+--
+
+CREATE TABLE `oauth_auth_codes` (
+  `id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `scopes` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `revoked` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `oauth_clients`
+--
+
+CREATE TABLE `oauth_clients` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `secret` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `redirect` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `personal_access_client` tinyint(1) NOT NULL,
+  `password_client` tinyint(1) NOT NULL,
+  `revoked` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 转存表中的数据 `oauth_clients`
+--
+
+INSERT INTO `oauth_clients` (`id`, `user_id`, `name`, `secret`, `redirect`, `personal_access_client`, `password_client`, `revoked`, `created_at`, `updated_at`) VALUES
+(1, NULL, 'Lipstick Personal Access Client', 'cWPdmc32qDy1kXVW3IwQ1LtdL4j6GWB6GE7uCXOw', 'http://localhost', 1, 0, 0, '2018-11-27 09:56:30', '2018-11-27 09:56:30'),
+(2, NULL, 'Lipstick Password Grant Client', 'MNPQLoTua2OMtGUxeBVsjSKRYYm6U2lYboJjgDA1', 'http://localhost', 0, 1, 0, '2018-11-27 09:56:30', '2018-11-27 09:56:30');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `oauth_personal_access_clients`
+--
+
+CREATE TABLE `oauth_personal_access_clients` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 转存表中的数据 `oauth_personal_access_clients`
+--
+
+INSERT INTO `oauth_personal_access_clients` (`id`, `client_id`, `created_at`, `updated_at`) VALUES
+(1, 1, '2018-11-27 09:56:30', '2018-11-27 09:56:30');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `oauth_refresh_tokens`
+--
+
+CREATE TABLE `oauth_refresh_tokens` (
+  `id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `access_token_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `revoked` tinyint(1) NOT NULL,
+  `expires_at` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_config`
+--
+ALTER TABLE `admin_config`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `admin_config_name_unique` (`name`);
+
+--
+-- Indexes for table `admin_menu`
+--
+ALTER TABLE `admin_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admin_operation_log`
+--
+ALTER TABLE `admin_operation_log`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `admin_operation_log_user_id_index` (`user_id`);
+
+--
+-- Indexes for table `admin_permissions`
+--
+ALTER TABLE `admin_permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `admin_permissions_name_unique` (`name`);
+
+--
+-- Indexes for table `admin_roles`
+--
+ALTER TABLE `admin_roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `admin_roles_name_unique` (`name`);
+
+--
+-- Indexes for table `admin_role_menu`
+--
+ALTER TABLE `admin_role_menu`
+  ADD KEY `admin_role_menu_role_id_menu_id_index` (`role_id`,`menu_id`);
+
+--
+-- Indexes for table `admin_role_permissions`
+--
+ALTER TABLE `admin_role_permissions`
+  ADD KEY `admin_role_permissions_role_id_permission_id_index` (`role_id`,`permission_id`);
+
+--
+-- Indexes for table `admin_role_users`
+--
+ALTER TABLE `admin_role_users`
+  ADD KEY `admin_role_users_role_id_user_id_index` (`role_id`,`user_id`);
+
+--
+-- Indexes for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `admin_users_username_unique` (`username`);
+
+--
+-- Indexes for table `admin_user_permissions`
+--
+ALTER TABLE `admin_user_permissions`
+  ADD KEY `admin_user_permissions_user_id_permission_id_index` (`user_id`,`permission_id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `oauth_access_tokens`
+--
+ALTER TABLE `oauth_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `oauth_access_tokens_user_id_index` (`user_id`);
+
+--
+-- Indexes for table `oauth_auth_codes`
+--
+ALTER TABLE `oauth_auth_codes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `oauth_clients`
+--
+ALTER TABLE `oauth_clients`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `oauth_clients_user_id_index` (`user_id`);
+
+--
+-- Indexes for table `oauth_personal_access_clients`
+--
+ALTER TABLE `oauth_personal_access_clients`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `oauth_personal_access_clients_client_id_index` (`client_id`);
+
+--
+-- Indexes for table `oauth_refresh_tokens`
+--
+ALTER TABLE `oauth_refresh_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `oauth_refresh_tokens_access_token_id_index` (`access_token_id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `admin_config`
+--
+ALTER TABLE `admin_config`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用表AUTO_INCREMENT `admin_menu`
+--
+ALTER TABLE `admin_menu`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- 使用表AUTO_INCREMENT `admin_operation_log`
+--
+ALTER TABLE `admin_operation_log`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- 使用表AUTO_INCREMENT `admin_permissions`
+--
+ALTER TABLE `admin_permissions`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- 使用表AUTO_INCREMENT `admin_roles`
+--
+ALTER TABLE `admin_roles`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用表AUTO_INCREMENT `admin_users`
+--
+ALTER TABLE `admin_users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用表AUTO_INCREMENT `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- 使用表AUTO_INCREMENT `oauth_clients`
+--
+ALTER TABLE `oauth_clients`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用表AUTO_INCREMENT `oauth_personal_access_clients`
+--
+ALTER TABLE `oauth_personal_access_clients`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用表AUTO_INCREMENT `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
